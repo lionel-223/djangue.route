198ea7ec0bb9360c7a7109e19c6c3dd71910194a
Tina <me@tina.moe>
2021-12-08 16:09:26 +0100

    Remove language name from db

--- app/models/language.py
+++ app/models/language.py
@@ -8,6 +8,5 @@ class Language(db.TimedMixin, db.Base):
     # English = "en", we do not distinguis between British, American, Australian
     # See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
     code = sa.Column(sa.String, nullable=False, primary_key=True)
-    name = sa.Column(sa.String, nullable=False)
     accepts_letters = sa.Column(sa.Boolean)
     has_translations = sa.Column(sa.Boolean)
