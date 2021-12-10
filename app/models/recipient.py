f5a07a92b525a375e5348824aa06ad04e24981d4
Tina <me@tina.moe>
2021-12-10 17:38:20 +0100

    Add recipient type

--- app/models/recipient.py
+++ app/models/recipient.py
@@ -1,10 +1,22 @@
 import sqlalchemy as sa
+from sqlalchemy import orm
 
 from app import db
+
+
+class RecipientType(db.IdMixin, db.Base):
+    def __str__(self):
+        return {
+            1: 'EHPAD',
+            2: 'Asso',
+        }.get(self.id)
 
 
 class Recipient(db.IdMixin, db.TimedMixin, db.LocationMixin, db.Base):
     email = sa.Column(sa.String)
     receive_letters = sa.Column(sa.Boolean)
+    type_id = sa.Column(sa.ForeignKey('recipient_type.id'))
     # frequency
     # date
+
+    type = orm.relationship('RecipientType', backref='recipients')
