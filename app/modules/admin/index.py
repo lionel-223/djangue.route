541bb07ad002f6e393431837892d420172c2a257
benmion <benmion@gmail.com>
2022-01-04 18:10:17 +0100

    Add moderation interface

--- app/modules/admin/index.py
+++ app/modules/admin/index.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from flask import render_template
+from flask import render_template, request
 
 from app import db
 from app.models import Letter
@@ -24,6 +24,13 @@ def index():
     return render_template('admin/dashboard.html', stats=stats)
 
 
-@bp.route('/moderation')
+@bp.route('/moderation', methods=['GET', 'POST'])
 def moderation():
-    return 'Modération'
+    new_status = request.form.get('status', None)
+    letter_id = request.form.get('letter_id', None)
+    if letter_id:
+        letter = db.session.query(Letter).get(letter_id)
+        letter.status = new_status
+        db.session.commit()
+    new_letter = db.session.query(Letter).filter(Letter.status == "not_moderated").order_by(Letter.created_at).first()
+    return render_template('admin/moderation.html', letter=new_letter, post=request.form)
