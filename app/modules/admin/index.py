8338a8a359a4bd5df82bb4010d300efcdcef60b1
benmion <benmion@gmail.com>
2022-01-13 15:38:10 +0100

    Fix stat mistake

--- app/modules/admin/index.py
+++ app/modules/admin/index.py
@@ -30,15 +30,16 @@ def index():
         'ehpad_count': db.session.query(Recipient).filter_by(type=Recipient.Types.retirement_home).count(),
         'associations_count': db.session.query(Recipient).filter_by(type=Recipient.Types.association).count(),
     }
-    user_moderation_count = len(current_user.moderated_letters)
-    user_moderation_week_count = db.session.query(Letter).filter((Letter.moderator == current_user)
-                                            & (Letter.moderation_time >= week_start)).count()
-    user_accepted_count = db.session.query(Letter).filter_by(moderator=current_user,
-                                                             status=Letter.Status.approved).count()
-    if user_moderation_count > 0:
-        stats['user_moderation_count'] = user_moderation_count
+    user_moderated_letters = db.session.query(Letter).filter((Letter.moderator == current_user) &
+                                                            (Letter.status.in_([Letter.Status.approved,
+                                                                                Letter.Status.rejected])
+                                                             ))
+    user_moderation_week_count = user_moderated_letters.filter(Letter.moderation_time >= week_start).count()
+    user_accepted_count = user_moderated_letters.filter_by(status=Letter.Status.approved).count()
+    if user_moderated_letters.count() > 0:
+        stats['user_moderation_count'] = user_moderated_letters.count()
         stats['user_moderation_week_count'] = user_moderation_week_count
-        stats['user_pct_accepted'] = round(100 * user_accepted_count / user_moderation_count)
+        stats['user_pct_accepted'] = round(100 * user_accepted_count / user_moderated_letters.count())
     return render_template('admin/dashboard.html', stats=stats)
 
 
