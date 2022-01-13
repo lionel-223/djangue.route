df388c458dd5279278cc9e04f76c38bc28925cb5
benmion <benmion@gmail.com>
2022-01-13 15:18:00 +0100

    Merge alembic heads

---
+++ alembic/versions/bebe1f57b08d_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge_heads
+
+Revision ID: bebe1f57b08d
+Revises: d46357679405, dc6c5c2efabc
+Create Date: 2022-01-13 15:16:42.209849
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bebe1f57b08d'
+down_revision = ('d46357679405', 'dc6c5c2efabc')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
