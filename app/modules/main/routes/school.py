eed9a8ce318cf729ee40a5d054dda138e1637cd9
benmion <benmion@gmail.com>
2022-02-24 11:47:22 +0100

    Allow teacher to join existing school

--- app/modules/main/routes/school.py
+++ app/modules/main/routes/school.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_user, login_required
 
 from app import db
@@ -26,6 +26,35 @@ def register_school():
     db.session.commit()
     flash('Inscription réussie !')
     return redirect(url_for('main.teacher_home'))
+
+
+@bp.route('/schools/join/', methods=['GET', 'POST'])
+@login_required
+def join_existing_school():
+    if request.method == 'GET':
+        return render_template('join_school.html')
+    school_id = request.values.get('school_id')
+    school = db.session.get(School, school_id)
+    if not school:
+        flash('This school does not exist !')
+        return render_template('join_school.html')
+    if school in current_user.schools:
+        flash('You already belong to this school !')
+        return render_template('join_school.html')
+    current_user.schools.append(school)
+    db.session.commit()
+    flash('Inscription réussie !')
+    return redirect(url_for('main.teacher_home'))
+
+
+@bp.get('/schools/search/')
+def schools_search():
+    search = request.args.get('term')
+    if not search:
+        return {'results': []}
+    results = db.session.query(School).filter(School.name.ilike(f'%{search}%'))
+    response = {'results': [{'id': school.id, 'text': school.name} for school in results]}
+    return response
 
 
 @bp.route('/schools/remove_current_user/<int:school_id>')
