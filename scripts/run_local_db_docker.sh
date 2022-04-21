0a9eab0c10103be59de42a2684dba45ea1d039f4
Damien <me@tina.moe>
2022-04-21 03:09:01 +0200

    Fix run db script crashing if container exists

--- scripts/run_local_db_docker.sh
+++ scripts/run_local_db_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name postgres_1l1s -p 5435:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -v /var/lib/postgresql/data postgres
+docker run --replace -d --name postgres_1l1s -p 5435:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -v /var/lib/postgresql/data postgres
