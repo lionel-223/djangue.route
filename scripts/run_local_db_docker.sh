87b5b76e92827fdb14b87890f7f654e8e37ac061
Damien <me@tina.moe>
2022-06-07 17:59:21 +0200

    Edit run_local_db_docker.sh to not recreate container if it already exists

--- scripts/run_local_db_docker.sh
+++ scripts/run_local_db_docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --replace -d --name postgres_1l1s -p 5435:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -v /var/lib/postgresql/data postgres
+name=postgres_1l1s
+
+if docker container exists $name; then
+	docker start $name
+else
+	docker run --replace -d --name $name -p 5435:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -v /var/lib/postgresql/data postgres
+fi
